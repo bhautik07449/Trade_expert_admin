ff9f26c041ed8a5cd9bae087c77898781228b799
bhautik07449 <bhautikpokal7449@gmail.com>
2025-12-26 21:00:18 +0530

    only for show in leptop screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 import "./App.css";
 import Router from "./router/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 
 function App() {
@@ -25,11 +25,24 @@ function App() {
     }
   }, []);
 
+  const [isLaptop, setIsLaptop] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLaptop(window.innerWidth >= 1024);
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
 
   return (
     <>
-      <Router isLoggedIn={isLoggedIn} />
+      {isLaptop ? <Router isLoggedIn={isLoggedIn} /> : <div className='flex justify-center items-center h-screen '>This website is accessible only on laptop and desktop devices.</div>}
     </>
   );
 }
