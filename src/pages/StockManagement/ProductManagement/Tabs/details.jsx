c955b868fa10d01a9dade2fc65fb9a1c2631a3f4
bhautik07449 <bhautikpokal7449@gmail.com>
2026-01-28 22:12:04 +0530

    editor set

--- src/pages/StockManagement/ProductManagement/Tabs/details.jsx
+++ src/pages/StockManagement/ProductManagement/Tabs/details.jsx
@@ -1,7 +1,22 @@
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useState } from 'react';
+
 export default function Details() {
+    const [editorData, setEditorData] = useState('');
+
+    const handleEditorChange = (event, editor) => {
+        const data = editor.getData();
+        setEditorData(data);
+    };
+
     return (
         <div>
-            Details Tab Content
+            <CKEditor
+                editor={ClassicEditor}
+                data={editorData}
+                onChange={handleEditorChange}
+            />
         </div>
     );
 }
