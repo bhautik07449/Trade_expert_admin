c97861582a1a64fb1b26787f96225abaa415f433
bhautik07449 <bhautikpokal7449@gmail.com>
2026-03-15 11:08:51 +0530

    buyer

--- src/pages/BuyerInteraction/BuyerManagement/BuyerManagement.jsx
+++ src/pages/BuyerInteraction/BuyerManagement/BuyerManagement.jsx
@@ -16,7 +16,7 @@ const columns = [
     { field: "phone", headerName: "Phone", flex: 3 },
     {
         field: "status", headerName: "Status", flex: 2, renderCell: (params) => (
-            <span className={`px-3 py-1 rounded-full text-sm font-medium ${getStatusStyles(params.value)}`}>
+            <span className={`px-3 py-1 rounded-full text-sm font-medium capitalize ${getStatusStyles(params.value)}`}>
                 {params.value}
             </span>
         )
