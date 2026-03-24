cd1908c1c25301586b357c6618be0d43053bbb75
bhautik07449 <bhautikpokal7449@gmail.com>
2026-03-24 23:54:07 +0530

    credit account api set

--- src/pages/BuyerInteraction/CreditAccountManagement/CreditAccountManagement.jsx
+++ src/pages/BuyerInteraction/CreditAccountManagement/CreditAccountManagement.jsx
@@ -1,29 +1,48 @@
 import { CommonTextField } from "../../../components/widgets/common_textField";
 import { Card } from "../../../components/ui/card";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CommonTable from "../../../components/widgets/common_table";
 import ExportData from "../../../components/widgets/export_data";
 import CommonFiltter from "../../../components/widgets/common_filter";
-
-const DmrList = [
-    { SrNo: "1", name: "Admin 1", category: "Agri & Foods", sub_category: "Fresh Produces", Status: "Active", Created: "14/11/2023" },
-    { SrNo: "2", name: "Admin 2", category: "Agri & Foods", sub_category: "Fresh Produces", Status: "Deactive", Created: "14/11/2023" },
-    { SrNo: "3", name: "Admin 3", category: "Agri & Foods", sub_category: "Fresh Produces", Status: "Active", Created: "14/11/2023" },
-    { SrNo: "4", name: "Admin 4", category: "Agri & Foods", sub_category: "Fresh Produces", Status: "Deactive", Created: "14/11/2023" },
-]
+import Creditaccountservice from "../../../service/creditaccount.service";
+import { formatDate } from "../../../common/constants";
+import { toast } from "../../../components/ui/use-toast";
 
 const columns = [
     { field: "SrNo", headerName: "SrNo", flex: 1 },
-    { field: "name", headerName: "name", flex: 1 },
-    { field: "category", headerName: "category", flex: 1 },
-    { field: "sub_category", headerName: "sub_category", flex: 1 },
-    { field: "Status", headerName: "Status", flex: 1 },
-    { field: "Created", headerName: "Created", flex: 1 },
+    { field: "yourName", headerName: "name", flex: 1 },
+    { field: "yourEmail", headerName: "Email", flex: 1 },
+    { field: "yourPosition", headerName: "Position", flex: 1 },
+    { field: "companyName", headerName: "Company Name", flex: 1 },
+    { field: "country", headerName: "Country", flex: 1 },
+    { field: "createdAt", headerName: "Created", flex: 1 },
 ]
 
 export default function CreditAccountManagement() {
+    const [list, setList] = useState([])
     const [search, setSearch] = useState("");
     console.log("search", search);
+
+    const getData = async () => {
+        try {
+            const res = await Creditaccountservice.getList()
+            if (res) {
+                const formattedData = res?.data?.accounts?.map((item, index) => ({
+                    ...item,
+                    SrNo: index + 1,
+                    createdAt: formatDate(item?.createdAt),
+                }))
+
+                setList(formattedData)
+            }
+        } catch (error) {
+            toast.error("Inquiry not send")
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
 
     const filterData = [
         { type: "text", placeholder: "Buyer Name", label: "Buyer Name" },
@@ -37,6 +56,28 @@ export default function CreditAccountManagement() {
 
     const handleClearFilters = () => {
         console.log("Filters Cleared");
+    }
+
+    const handleDelete = async (id) => {
+        try {
+
+            const res = await Creditaccountservice.deleteCreditaccount(id)
+            if (res) {
+                toast({
+                    variant: "success",
+                    title: "Inquiry",
+                    description: res?.data?.message,
+                });
+                getData()
+            }
+
+        } catch (error) {
+            toast({
+                variant: "error",
+                title: "Inquiry",
+                description: error.message || "Inquiry not Deleted.",
+            });
+        }
     }
 
     return (
@@ -58,7 +99,7 @@ export default function CreditAccountManagement() {
                     </div>
                     <div className="flex gap-4">
                         <ExportData
-                            data={DmrList}
+                            data={list}
                             fileName="credit_account_data.xlsx"
                         />
                         <CommonFiltter
@@ -71,11 +112,9 @@ export default function CreditAccountManagement() {
 
                 <CommonTable
                     columns={columns}
-                    rows={DmrList || []}
-                    showEdit={true}
+                    rows={list || []}
                     showDelete={true}
-                    onEdit={() => { }}
-                    onDelete={() => { }}
+                    onDelete={handleDelete}
                 />
             </Card>
         </div>
---
+++ src/service/creditaccount.service.jsx
@@ -0,0 +1,52 @@
+import serverCall from "../serverCall";
+
+const getList = async () => {
+    try {
+        const response = serverCall.get('/creditaccount')
+        return response
+    } catch (error) {
+        throw error
+    }
+}
+
+const getByid = async (id) => {
+    try {
+        const response = serverCall.get(`/creditaccount/${id}`)
+        return response
+    } catch (error) {
+        throw error
+    }
+}
+
+const addCreditaccount = async (body) => {
+    try {
+        const response = await serverCall.post('/creditaccount', body);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const updateCreditaccount = async (id, body) => {
+    try {
+        const response = await serverCall.patch(`/creditaccount/${id}`, body);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const deleteCreditaccount = async (id) => {
+    try {
+        const response = await serverCall.delete(`/creditaccount/${id}`);
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const Creditaccountservice = {
+    getList, addCreditaccount, getByid, updateCreditaccount, deleteCreditaccount
+};
+
+export default Creditaccountservice;
