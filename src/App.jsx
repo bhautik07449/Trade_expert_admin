6becfbe43f6cbb5acd1c96cd59a892bb799087f7
bhautik07449 <bhautikpokal7449@gmail.com>
2026-05-13 14:22:23 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setLoggedIn } from "./store/slice/auth";
 
-
 function App() {
 
   useEffect(() => {
